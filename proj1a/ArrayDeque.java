498671a90a24036abed309968a4558abd1159031
philipzhang86 <xiaoxiaoxiao67@gmail.com>
2021-07-13 04:56:30 -0500

    All finished ninth attempt

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,8 +1,8 @@
 public class ArrayDeque<T> {
-    public T[] items;
-    public int size;
-    public int front;
-    public int tail;
+    private T[] items;
+    private int size;
+    private int front;
+    private int tail;
 
     public ArrayDeque() {
         items = (T[]) new Object[8];
@@ -16,18 +16,18 @@ public class ArrayDeque<T> {
     }
 
     private int subOne(int a) {
-        return (a - 1 + items.length) % items.length;// 0前面再加 即刻变成index7   //不断往前加
+        return (a - 1 + items.length) % items.length ;// 0前面再加 即刻变成index7   //不断往前加
         //7-1+8=>14 % 8 =>6 再往前就 下一个index为6
     }
 
-    public void resize(int length) {
+    private void resize(int length) {
         T[] newItems = (T[]) new Object[length];
         int oldIndex = addOne(front);
         System.arraycopy(items, 0, newItems, 0, size);
         items = newItems;
         //原数组0~7复制到长度为16的新数组，那么新数组的头就是index15
         front = items.length - 1;
-        tail = size;//新数组的尾就从index 8开始，因为0~7已经有元素了
+        tail = size; //新数组的尾就从index 8开始，因为0~7已经有元素了
     }
 
     public void addFirst(T item) {
@@ -63,7 +63,7 @@ public class ArrayDeque<T> {
         int i = addOne(front);
         for (int j = 0; j < size; j++) {
             System.out.print(items[i] + " ");
-            i = addOne(i);//print完之后继续指针移到下一个格
+            i = addOne(i); //print完之后继续指针移到下一个格
         }
     }
 
