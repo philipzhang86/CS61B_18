6dd5443cf7763848263cb4147cd9b77a2fead55d
philipzhang86 <xiaoxiaoxiao67@gmail.com>
2021-07-18 18:12:44 -0500

    All finished thirteen attempt

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,131 +1,162 @@
 public class ArrayDeque<T> {
+    private T[] items;
     private int size;
     private int front;
     private int tail;
-    private T[] items;
-
 
     public ArrayDeque() {
         items = (T[]) new Object[8];
         size = 0;
+        front = 4;
+        tail = 5;
     }
 
+    private void resize(int capacity) {
+        T[] newCopy = (T[]) new Object[capacity];
+        if (front < tail) {
+            System.arraycopy(items, front + 1, newCopy, front + 1, size);
+        } else {
+            if (tail > 0) {
+                System.arraycopy(items, 0, newCopy, 0, tail);
+            }
+            if (front < items.length - 1) {
+                System.arraycopy(items, front + 1, newCopy, capacity - items.length + front + 1,
+                        items.length - front - 1);
+            }
+            front = front + capacity - items.length;
+        }
+        items = newCopy;
+    }
+
+    private void halfSize(int capacity) {
+        T[] newCopy = (T[]) new Object[capacity];
+        if (front < tail) {
+            System.arraycopy(items, front + 1, newCopy, 2, size);
+            items = newCopy;
+            front = 1;
+            tail = size + 2;
+        } else {
+            if (tail > 0) {
+                System.arraycopy(items, 0, newCopy, 0, tail);
+            }
+            if (front < items.length - 1) {
+                System.arraycopy(items, front + 1, newCopy,
+                        capacity - items.length + front + 1, items.length - front - 1);
+            }
+            front = front - items.length + capacity;
+            items = newCopy;
+        }
+    }
+
+    public void addLast(T element) {
+        if (element == null) {
+            return;
+        }
+        if (size + 2 == items.length) {
+            resize(items.length * 2);
+        }
+        items[tail] = element;
+        if (tail == items.length - 1) {
+            tail = 0;
+        } else {
+            tail++;
+        }
+        size++;
+    }
+
+    public void addFirst(T element) {
+        if (element == null) {
+            return;
+        }
+        if (size + 2 == items.length) {
+            resize(items.length * 2);
+        }
+        items[front] = element;
+
+        if (front == 0) {
+            front = items.length - 1;
+        } else {
+            front--;
+        }
+        size++;
+    }
 
     public boolean isEmpty() {
         return size == 0;
     }
 
-    private void resize(int capacity) {
-        T[] newArray = (T[]) new Object[capacity];
-        System.arraycopy(items, 0, newArray, 0, size);
-        items = newArray;
-    }
-
-    public void addFirst(T n) {
-        if (n == null) {
-            return;
-        }
-
-        items[front = (front - 1) & (items.length - 1)] = n;
-        if (front == tail) {
-            doubleCapacity();
-        }
-        size++;
-    }
-
-    public void addLast(T n) {
-        if (n == null) {
-            return;
-        }
-
-        items[tail] = n;
-        // tail指针加1，如果到数组尾了就从头开始
-        if ((tail = (tail + 1) & (items.length - 1)) == front) {
-            doubleCapacity();
-        }
-        size++;
-    }
-
-    public T removeFirst() {
-        if (isEmpty()) {
-            System.out.println("Deque is empty, you can't remove elements");
-            return null;
-        }
-        T result = items[front];
-        items[front] = null;
-        front = (front + 1) & (items.length - 1);
-        size--;
-        if (isLowUsageRate()) {
-            resize((int) (items.length * 0.5));
-        }
-        return result;
-    }
-
-    public T removeLast() {
-        if (isEmpty()) {
-            System.out.println("Deque is empty, you can't remove elements");
-            return null;
-        }
-        int t = (tail - 1) & (items.length - 1);
-        T result = items[t];
-        items[t] = null;
-        tail = t;
-        size--;
-        if (isLowUsageRate()) {
-            resize((int) (items.length * 0.5));
-        }
-        return result;
-    }
-
-    public T get(int index) {
-        if (isEmpty() || index < 0 || index >= size) {
-            return null;
-        }
-        return items[(front + index) % items.length];
-    }
-
-
     public int size() {
         return size;
     }
 
-    public void printDeque() {
-        if (isEmpty()) {
-            return;
+    public T removeFirst() {
+        if (items.length >= 16 && size * 4 == items.length) {
+            halfSize(items.length / 2);
         }
+        if (isEmpty()) {
+            return null;
+        }
+        if (front == items.length - 1) {
+            front = 0;
+        } else {
+            front = front + 1;
+        }
+        T temp = items[front];
+        items[front] = null;
+        size--;
+
+        return temp;
+    }
+
+    public T removeLast() {
+        if (items.length >= 16 && size * 4 == items.length) {
+            halfSize(items.length / 2);
+        }
+        if (isEmpty()) {
+            return null;
+        }
+
+        if (tail == 0) {
+            tail = items.length - 1;
+        } else {
+            tail--;
+        }
+        size--;
+        T temp = items[tail]; //指针已经减了，所以才会不用再减
+        items[tail] = null;
+        return temp;
+    }
+
+    public T get(int index) {
+        if (index >= 0 && index < size) {
+            return items[(front + 1 + index) % items.length];
+        }
+        return null;
+    }
+
+    public void printDeque() {
 
         if (front < tail) {
-            for (int i = 0; i != tail; i++) {
-                System.out.print(items[i] + " ");
+            int c = front + 1;
+            while (!(c == tail)) {
+                System.out.print(items[c] + " ");
+                c = c + 1;
+            }
+        } else {
+            if (front < items.length - 1) {
+                int c = front + 1;
+                while (!(c == items.length)) {
+                    System.out.print(items[c] + " ");
+                    c = c + 1;
+                }
+            }
+            if (tail > 0) {
+                int c = 0;
+                while (!(c == tail)) {
+                    System.out.print(items[c] + " ");
+                    c = c + 1;
+                }
             }
         }
-
-        if (front > tail) {
-            for (int i = front; i <= items.length - 1; i++) {
-                System.out.print(items[i] + " ");
-            }
-            for (int i = 0; i < tail; i++) {
-                System.out.print(items[i] + " ");
-            }
-        }
-    }
-
-    private void doubleCapacity() {
-        assert front == tail;
-        int p = front;
-        int n = items.length;
-        int r = n - p;
-        int newCapacity = n * 2;
-
-        T[] newArray = (T[]) new Object[newCapacity];
-        System.arraycopy(items, p, newArray, 0, r);
-        System.arraycopy(items, 0, newArray, r, p);
-        items = newArray;
-        front = 0;
-        tail = n;
-    }
-
-    private boolean isLowUsageRate() {
-        return items.length >= 16 && size < (items.length * 0.25);
     }
 }
