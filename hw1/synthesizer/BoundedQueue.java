2b34218ddd3bc9d152fd44b5f064120ff3bccf37
philipzhang86 <xiaoxiaoxiao67@gmail.com>
2021-07-27 16:42:17 -0500

    First Attempt

--- hw1/synthesizer/BoundedQueue.java
+++ hw1/synthesizer/BoundedQueue.java
@@ -1,12 +1,12 @@
 package synthesizer;
 
 public interface BoundedQueue<T> extends Iterable<T> {
-    int capacity();//has a fixed capacity
+    int capacity(); //has a fixed capacity
 
-    int fillCount();// nothing is allowed to enqueue if the queue is full
+    int fillCount(); // nothing is allowed to enqueue if the queue is full
 
     //items can only be enqueued at the back of the queue,
-    void enqueue(T x);// addTail
+    void enqueue(T x); // addTail
 
     //can only be dequeued from the front of the queue.
     T dequeue(); //removeFirst
