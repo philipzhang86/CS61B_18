661e1cd9ca0e67f97483cff88ea04b45f61cc672
philipzhang86 <xiaoxiaoxiao67@gmail.com>
2021-08-02 20:14:10 -0500

    First attempt

--- proj2/byog/Core/Game.java
+++ proj2/byog/Core/Game.java
@@ -1,6 +1,5 @@
 package byog.Core;
 
-import byog.TileEngine.TERenderer;
 import byog.TileEngine.TETile;
 import byog.TileEngine.Tileset;
 
@@ -8,7 +7,6 @@ public class Game {
     /* Feel free to change the width and height. */
     public static final int WIDTH = 80;
     public static final int HEIGHT = 30;
-
 
     private TETile[][] worldInitialize(String s) {
         // initialize tiles
@@ -47,10 +45,7 @@ public class Game {
         // TODO: Fill out this method to run the game using the input passed in,
         // and return a 2D tile representation of the world that would have been
         // drawn if the same inputs had been given to playWithKeyboard().
-        long seed = Long.parseLong(input);
-        //long seed = Long.parseLong(input.replaceAll("[^0-9]*$", ""));
-        TERenderer ter = new TERenderer();
-        ter.initialize(WIDTH, HEIGHT);
+        long seed = Long.parseLong(input.replaceAll("[^0-9]", ""));
         MapGenerator map = new MapGenerator(WIDTH, HEIGHT, seed);
         return map.mapGenerator();
     }
